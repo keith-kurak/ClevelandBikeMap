7e23cf7c5da1e4848983436f1a7b9560984dddb8
Keith Kurak <keith.kurak@gmail.com>
2014-07-27 10:28:25 -0400

    Initial Commit

---
+++ ClevelandBikeMap/CBMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CBMAppDelegate.h
+//  ClevelandBikeMap
+//
+//  Created by Keith on 7/27/14.
+//  Copyright (c) 2014 Keith Kurak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClevelandBikeMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClevelandBikeMap
+//
+//  Created by Keith on 7/27/14.
+//  Copyright (c) 2014 Keith Kurak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CBMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CBMAppDelegate class]));
+    }
+}
